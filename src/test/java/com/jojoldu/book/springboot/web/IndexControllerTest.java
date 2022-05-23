6ddb00f2fb9d4a1b04a0a4e9fb3f892f475b768c
BYUNGHOON PARK <i.am.planner@gmail.com>
2022-05-23 11:14:00 +0900

    메인페이지_로딩 테스트 케이스 수정

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -22,7 +22,8 @@ public class IndexControllerTest {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
+        System.out.println(body);
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트 웹서비스");
     }
 }
